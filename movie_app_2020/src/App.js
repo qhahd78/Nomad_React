22966fdb4a5b3fe76699cd05fc48bedfaceac7b0
qhahd78 <umuj0708@likelion.org>
2021-02-02 01:09:51 +0900

    movie app  1

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,46 +1,25 @@
 import React from 'react';
-
+import axios from 'axios';
 // React.Component 클래스를 상속 받음.
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello');
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = () => {
+    const movies = axios.get('http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=f6649d26347bb450475cfb25dee1dd41');
   }
-
-  state ={
-    count: 0,
-  };
-
-  add= () =>{
-    // current 에는 현재 state 가 넘어온다. 
-    this.setState(current => ({
-      count: current.count +1,
-    }));
-  };
-
+  // 영화 데이터 로딩
   componentDidMount(){
-    console.log('component rendered');
+    this.getMovies();
   }
-
-  componentDidUpdate(){
-    console.log('I just updated');
-  }
-
-  minus = () => {
-    this.setState(current =>({
-      count: current.count-1,
-    }));
-  };
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>  
-    );
+    const { isLoading } = this.state;
+    // 삼항 연산자. isLoading 이 true 면 왼쪽을, false 면 오른쪽을 반환한다. 
+    // We are ready 부분에 영화 데이터 출력. 
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
